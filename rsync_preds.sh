eea7c7e66e301464ea40192288a3e28f6dc2e000
Jim White <jim@ifcx.org>
2025-01-31 16:18:07 -0800

    Snapshot JS for keeping Colab alive

--- rsync_preds.sh
+++ rsync_preds.sh
@@ -9,3 +9,24 @@ while true; do
   ls -laot predictions/terse* | head
   sleep 600 # Sleep for 10 minutes (600 seconds)
 done
+
+
+<<KEEPALIVE
+
+https://gist.github.com/pouyaardehkhani/29a59270801a209d4960e2aefe648bbc
+
+function ClickConnect() {
+  console.log('Working')
+  document
+    .querySelector('#top-toolbar > colab-connect-button')
+    .shadowRoot.querySelector('#connect')
+    .click()
+}
+intervalTiming = setInterval(ClickConnect, 60000)
+
+
+Trying to bring focus back to terminal but haven't figured out how to do it yet
+document.querySelector("colab-xterm-terminal").shadowRoot.querySelector("textarea")
+
+document.querySelector("colab-xterm-terminal").shadowRoot.querySelector("xterm-screen").focus()
+>>KEEPALIVE
